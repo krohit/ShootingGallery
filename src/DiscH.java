19d7987d433cd6bd18fa6abfde41c91cd29bcaa6
Rohit K <krohit@gmail.com>
2014-01-25 18:18:20 -0800

    Targets
    Moving Targets in moth horizontal and vertical. Have move function and
    bounce function embedded within. Frame size of graphics screen can only
    be 500 x 500. Frame size is hard coded, need to change this separately.

---
+++ src/DiscH.java
@@ -0,0 +1,22 @@
+
+public class DiscH extends Target {
+
+	public DiscH (double x, double y, double velocity, int side) {
+			this.x = x;
+			this.y = y;
+			this.velocity = velocity;
+			this.side = side;
+			this.theta = 0.0;
+	}
+
+	void move(){
+		x = x + (Math.cos(theta) * velocity);
+		if (x < 0 || x > 500)
+			bounce();
+	}
+	
+	void bounce (){
+		velocity = -velocity;
+		theta = Math.PI - theta;
+	}
+}
